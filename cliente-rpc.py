725b6b2fc5bc97af233a6340bf25b69acfd36806
Abner Santana <abnermuxah@gmail.com>
2021-03-24 20:27:34 -0300

    Delete cliente-rpc.py

--- cliente-rpc.py
+++
@@ -1,6 +0,0 @@
-#Client RPC
-from xmlrpc.client import ServerProxy
-cliente = ServerProxy('http://localhost:20064', allow_none=True)
-while True:
-    nome = input("Digite o nome para receber o número \n")
-    cliente.nome(nome)
